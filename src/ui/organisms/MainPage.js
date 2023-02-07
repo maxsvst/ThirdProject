db1711d5c78f8db62533af380e6c679aa402b45c
maxsvst <fioklin2015@mail.ru>
2023-02-07 15:39:47 +0300

    Андерлайн настроек при выборе и декомпозиция кода

--- src/ui/organisms/MainPage.js
+++ src/ui/organisms/MainPage.js
@@ -5,6 +5,15 @@ import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 
 import axios from "axios";
 
+import {
+  weatherUrl,
+  coordsUrl,
+  appId,
+  getCurrentPosition,
+  palableTemperature,
+  sunriseTime,
+  sunsetTime,
+} from "../../heplers/common";
 import WeatherData from "../molecules/WeatherData";
 import CurrentCity from "../molecules/CurrentCity";
 
@@ -16,44 +25,32 @@ export default function MainPage() {
   const [showSunriseTime, setShowSunriseTime] = useState(false);
   const [showSunsetTime, setShowSunsetTime] = useState(false);
 
-  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
-  const coordsUrl = "http://api.openweathermap.org/geo/1.0/reverse";
   const currentCity = data?.name;
 
   useEffect(() => {
+    const fetchData = async () => {
+      const position = await getCurrentPosition();
+
+      const data = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        appid: appId,
+      };
+
+      const location = await axios
+        .get(coordsUrl, { params: data })
+        .then((resp) => resp.data[0]);
+      getWeatherData(location.name);
+      setLocation(location.name);
+    };
     fetchData();
   }, []);
-
-  const fetchData = async () => {
-    const position = await getCurrentPosition();
-
-    const data = {
-      lat: position.coords.latitude,
-      lon: position.coords.longitude,
-      appid: "64496c89f6baf98bda6b5dbb308ea1c7",
-    };
-
-    const location = await axios
-      .get(coordsUrl, { params: data })
-      .then((resp) => resp.data[0]);
-    getWeatherData(location.name);
-    setLocation(location.name);
-  };
-
-  const getCurrentPosition = () => {
-    return new Promise((res, rej) => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => res(position),
-        (error) => rej(error)
-      );
-    });
-  };
 
   const getWeatherData = (city) => {
     const weatherData = {
       q: city,
       units: "metric",
-      appid: "64496c89f6baf98bda6b5dbb308ea1c7",
+      appid: appId,
     };
 
     axios
@@ -74,7 +71,7 @@ export default function MainPage() {
     setSavedLocations(updatedCityList);
   };
 
-  const addCityToSavedCities = () => {
+  const addCityToSavedCityList = () => {
     const isLocationAlreadyExists = savedLocations.filter(
       (item) => item.label === location
     );
@@ -91,6 +88,12 @@ export default function MainPage() {
   const onToggleSunriseTime = () => setShowSunriseTime(!showSunriseTime);
   const onToggleSunsetTime = () => setShowSunsetTime(!showSunsetTime);
 
+  const isPalableTemperatureVisible = showPalpableTemperature
+    ? palableTemperature
+    : "Ощутимая температура";
+  const isSunriseTimeVisible = showSunriseTime ? sunriseTime : "Время восхода";
+  const isSunsetTimeVisible = showSunsetTime ? sunsetTime : "Время заката";
+
   const Settings = () => {
     return (
       <PopupState variant="popover" popupId="demo-popup-menu">
@@ -101,10 +104,14 @@ export default function MainPage() {
             </Button>
             <Menu {...bindMenu(popupState)}>
               <MenuItem onClick={onTogglePelpableTemperature}>
-                Ощутимая температура
+                {isPalableTemperatureVisible}
               </MenuItem>
-              <MenuItem onClick={onToggleSunriseTime}>Время восхода</MenuItem>
-              <MenuItem onClick={onToggleSunsetTime}>Время заката</MenuItem>
+              <MenuItem onClick={onToggleSunriseTime}>
+                {isSunriseTimeVisible}
+              </MenuItem>
+              <MenuItem onClick={onToggleSunsetTime}>
+                {isSunsetTimeVisible}
+              </MenuItem>
             </Menu>
           </React.Fragment>
         )}
@@ -155,13 +162,15 @@ export default function MainPage() {
           type="text"
           placeholder="Введите ваш город"
           onKeyPress={searchLocation}
-          onChange={(e) => setLocation(e.target.value)}
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
         />
         <SavedCities />
       </div>
       <div className="flex flex-col items-center mt-20">
         <CurrentCity
-          addCityToSavedCities={addCityToSavedCities}
+          addCityToSavedCities={addCityToSavedCityList}
           currentCity={currentCity}
         />
         <WeatherData
